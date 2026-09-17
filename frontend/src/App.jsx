50580068f2aeaf6af0b58ad65519c39bfada21ea
kapilraj26 <kapilrajmanogaran26@gmail.com>
2026-09-17 08:14:44 +0530

    Fix login import case for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import {
   useNavigate
 } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 
